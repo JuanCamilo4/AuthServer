5c343a9bf2930649ba5e9ea767efad329fc3fd8f
Juan Camilo Rios Tarazona <juanr_0404@hotmail.com>
2023-02-03 16:07:14 -0500

    Inicio del proyecto

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+//Crar el servidor el express
+const app = express();
+
+app.get()
+
+app.listen( 4000, () => {
+    console.log('Servidor corriendo en puerto 4000');
+})
